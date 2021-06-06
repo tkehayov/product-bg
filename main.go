58b6ebaa5997abb92ef775e3345e5ec91e43c20f
tihomirkehayov <kehayov89@gmail.com>
2021-06-06 14:12:17 +0300

    init

--- main.go
+++ main.go
@@ -11,11 +11,16 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello World71!")
 
 }
+func hi(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hi")
+
+}
 
 func main() {
 
 	port := os.Getenv("PORT")
 	fmt.Println(port)
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/hi", hi)
 	http.ListenAndServe(":"+port, nil)
 }
